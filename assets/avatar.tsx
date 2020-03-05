dc157972e56c84de362e48203b7c3777059bc15b
Olivier Perrault <olivier.perrault@usherbrooke.ca>
2020-03-05 09:00:38 -0500

    libgdx test

---
+++ assets/avatar.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="Avatar" tilewidth="32" tileheight="32" tilecount="36" columns="9">
+ <image source="avatar_spritesheet.png" width="288" height="128"/>
+</tileset>
--- src/gdxtest/Game.java
+++ src/gdxtest/Game.java
@@ -3,6 +3,8 @@ package gdxtest;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 
 
 public class Game extends ApplicationAdapter {
@@ -10,6 +12,16 @@ public class Game extends ApplicationAdapter {
 	@Override
 	public void create() {
 		Gdx.graphics.setWindowedMode(256, 540);
+		var tiledMap = new TmxMapLoader().load("assets/tilemap_overworld.tmx");
+		for(var lay : tiledMap.getLayers())
+		{
+			for(var obj : lay.getObjects())
+			{
+				System.out.println(obj);
+				System.out.println(obj);
+			}
+		}
+		System.out.println();
 	}
 
 	@Override
